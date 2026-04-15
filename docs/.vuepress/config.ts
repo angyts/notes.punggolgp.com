0e6eb8625929ebdc2de262ce62f3aa2a66fd21c1
angyts <23327200+angyts@users.noreply.github.com>
2026-04-15 16:39:39 +0800

    change to notes

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,8 +4,8 @@ import { searchPlugin } from "@vuepress/plugin-search";
 import { getDirname, path } from "@vuepress/utils";
 import { glob } from "glob";
 
-let songFiles = glob
-  .sync("docs/songs/**/*.md")
+let noteFiles = glob
+  .sync("docs/notes/**/*.md")
   .map((f) => f.replace("docs", "").replace("index.md", ""));
 
 import { description } from "../../package.json";
@@ -14,13 +14,7 @@ const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   lang: "en-US",
-  // Global title in HTML <head>.
-  // If page has title (in frontmatter) or h1 then: <page title/h1> | <global title>
-  // e.g <title>Vuepress-DecapCMS-Netlify | VueDN</title>
-  title: "VueDN",
-  // Global description in in HTML <head>.
-  // If page has description (in frontmatter) then: <global description is replaced by <page description>
-  // <meta name="description" content="...">
+  title: "Medical Notes",
   description: description,
   head: [
     [
@@ -31,38 +25,33 @@ export default defineUserConfig({
     ],
   ],
 
-  // theme and its config
   theme: defaultTheme({
     logo: "vue.png",
-    notFound: ["There's nothing here. If you're looking for DecapCMS, manually enter `/admin` to the root site path to navigate directly to it."],
+    notFound: [
+      "There's nothing here. If you're looking for DecapCMS, manually enter `/admin` to the root site path.",
+    ],
+
     navbar: [
       {
-        text: "Songs",
-        // notice the trailing / (for the automatic next and prev links based on the sidebar)
-        link: "/songs/",
-      },
-      {
-        text: "Using this template",
-        link: "/template/",
+        text: "Medical Notes",
+        link: "/notes/",
       },
       {
         text: "GitHub",
         link: "https://github.com/NdagiStanley/VueDN",
       },
     ],
-    // notice there's a difference between /songs and /songs/
-    // We have the /songs to enable this sidebar for /songs and /songs/ paths
+
     sidebar: {
-      "/songs": [
+      "/notes": [
         {
-          text: "Songs",
-          children: songFiles,
+          text: "Medical Notes",
+          children: noteFiles,
         },
       ],
     },
   }),
 
-  // Replace footer
   alias: {
     "@theme/HomeFooter.vue": path.resolve(
       __dirname,
@@ -70,16 +59,10 @@ export default defineUserConfig({
     ),
   },
 
-  // plugin
   plugins: [
     registerComponentsPlugin({
-      // options
-      // Absolute path to the components directory
       componentsDir: path.resolve(__dirname, "./components"),
     }),
-    searchPlugin({
-      // options
-      // Default shortcut is key '/'
-    }),
+    searchPlugin({}),
   ],
 });
